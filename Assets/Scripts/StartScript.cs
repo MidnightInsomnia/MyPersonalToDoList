0d75a4d58b4abcafb443f5075774ece15e6910ef
TEXTOLEET <61066851+MidnightInsomnia@users.noreply.github.com>
2023-03-22 10:42:51 +0300

    StartScene + targetFramerate

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
